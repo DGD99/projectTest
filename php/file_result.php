7bd3b3c63dceda8ce8d9ddaf095b15322166e61d
DGD99 <mid7098@gmail.com>
2022-03-31 11:36:58 +0900

    파일 전송 php 완료

---
+++ php/file_result.php
@@ -0,0 +1,37 @@
+<?php
+	echo "파일 업로드 테스트";
+
+	if (isset($_FILES)) {
+    $file = $_FILES["file"];
+    $error = $file["error"];
+    $name = $file["name"];
+    $type = $file["type"];
+    $size = $file["size"];
+    $tmp_name = $file["tmp_name"];
+   
+    if ( $error > 0 ) {
+        echo "Error: " . $error . "<br>";
+    }
+    else {
+        $temp = explode(".", $name);
+        $extension = end($temp);
+       
+            echo "Upload: " . $name . "<br>";
+            echo "Type: " . $type . "<br>";
+            echo "Size: " . ($size / 1024 / 1024) . " Mb<br>";
+            echo "Stored in: " . $tmp_name;
+            if (file_exists("/upload/" . $name)) {
+                echo $name . " already exists. ";
+            }
+            else {
+                move_uploaded_file($tmp_name, "/upload/" . $name);
+                echo "Stored in: " . "/upload/" . $name;
+            }
+    }
+}
+else {
+    echo "File is not selected";
+}
+
+
+?>
