20e3b9c10145101e7e5bb9636aac0289179c065b
DGD99 <mid7098@gmail.com>
2022-03-31 12:11:54 +0900

    파일 전송 시 DB 로 입력

--- php/file_result.php
+++ php/file_result.php
@@ -1,37 +1,66 @@
 <?php
 	echo "파일 업로드 테스트";
 
+	$con=mysqli_connect("localhost","root","TEst!234","sqldb") or die("접속실패");
+
+
 	if (isset($_FILES)) {
-    $file = $_FILES["file"];
-    $error = $file["error"];
-    $name = $file["name"];
-    $type = $file["type"];
-    $size = $file["size"];
-    $tmp_name = $file["tmp_name"];
+    		$file = $_FILES["file"];
+    		$error = $file["error"];
+    		$name = $file["name"];
+    		$type = $file["type"];
+    		$size = $file["size"];
+    		$tmp_name = $file["tmp_name"];
    
-    if ( $error > 0 ) {
-        echo "Error: " . $error . "<br>";
-    }
-    else {
-        $temp = explode(".", $name);
-        $extension = end($temp);
+    		if ( $error > 0 ) {
+        	echo "Error: " . $error . "<br>";
+    		}
+    		else {
+        		$temp = explode(".", $name);
+        		$extension = end($temp);
        
-            echo "Upload: " . $name . "<br>";
-            echo "Type: " . $type . "<br>";
-            echo "Size: " . ($size / 1024 / 1024) . " Mb<br>";
-            echo "Stored in: " . $tmp_name;
-            if (file_exists("/upload/" . $name)) {
-                echo $name . " already exists. ";
-            }
-            else {
-                move_uploaded_file($tmp_name, "/upload/" . $name);
-                echo "Stored in: " . "/upload/" . $name;
-            }
-    }
-}
-else {
-    echo "File is not selected";
-}
+            		echo "Upload: " . $name . "<br>";
+            		echo "Type: " . $type . "<br>";
+            		echo "Size: " . (int)($size / 1024 / 1024) . " Mb<br>";
+            		echo "Stored in: " . $tmp_name;
+            		if (file_exists("/upload/" . $name)) {
+                		echo $name . " already exists. ";
+            		}	
+            		else {
+                		move_uploaded_file($tmp_name, "/upload/" . $name);
+                		echo "Stored in: " . "/upload/" . $name;
+				$sql="INSERT INTO filetbl VALUES(NULL,'".$name."','".$size."')";
+		
+				$ret = mysqli_query($con,$sql);            
+				if($ret) {
+                			echo "데이터 입력 완료";
+				}
+				else {
+                			echo "데이터 입력 실패"."<br>";
+                			echo "실패원인 : ".mysqli_error($con);
+	    			}
+	   		}
+?>
+	<script>
+		alert('업로드 성공');
+                //location.href = "page-alexa.html";
+	</script>
+<?php
+    	  }
+	}
+	else {
+    		echo "File is not selected";
+
+?>
+	<script>
+		alet('업로드 실패');
+                location.href = "page-alexa.html";
+	</script>
+<?php
+	
+	}	
+	
+	mysqli_close($con);
 
 
 ?>
